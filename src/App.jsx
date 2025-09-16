a08b7b56ee837c3df82090d5030f36b45bc8d7c6
royemosby <roy.e.mosby@gmail.com>
2025-09-16 10:09:49 -0400

    rename + update alt text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import templateLogo from './assets/images/pleasant-start.svg';
+import templateHero from './assets/images/pleasant-start.svg';
 import './App.css';
 
 function App() {
   return (
     <>
       <main>
-        <img src={templateLogo} className="logo" alt="React logo" />
+        <img
+          src={templateHero}
+          className="logo"
+          alt="Pleasant Start hero image"
+        />
       </main>
       <h1>Vite + React</h1>
     </>
